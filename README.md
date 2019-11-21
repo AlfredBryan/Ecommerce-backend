950bac913e0cad7e95e7db35e24b64b6d64de460
AlfredBryan <alfred.chimereze@gmail.com>
2019-11-21 09:50:12 +0100

    initial commit

--- README.md
+++
@@ -1,7 +0,0 @@
-# Full stack ecommerce online store application
-
-#### online demo [click here](https://zack-ecommerce-reactjs.herokuapp.com/)
-
-#### front-end codes [click here](https://github.com/levelopers/Ecommerce-Reactjs)
-
-#### api documentation:  [swaggerHub](https://app.swaggerhub.com/apis-docs/levelopers2/Ecommerce/1.0.0)
--- app.js
+++ app.js
@@ -1,57 +1,60 @@
-var createError = require('http-errors');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser')
-var logger = require('morgan');
-var express = require('express');
-var mongoose = require('mongoose');
-var cors = require('cors')
-var expressValidator  = require('express-validator');//req.checkbody()
-const mongoConfig = require('./configs/mongo-config')
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const createError = require("http-errors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const expressValidator = require("express-validator"); //req.checkbody()
+const mongoConfig = require("./configs/mongo-config");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+require("dotenv").config();
 
 //mongodb://heroku_8bd94qrf:irstf0rv1ds970eebtislm0apf@ds029638.mlab.com:29638/heroku_8bd94qrf
-mongoose.connect(mongoConfig, { useNewUrlParser: true, useCreateIndex: true, },function(error){
-  if(error) throw error
-    console.log(`connect mongodb success`);
-});
+mongoose.connect(
+  mongoConfig,
+  { useNewUrlParser: true, useCreateIndex: true },
+  error => {
+    if (error) throw error;
+    console.log(`connected to DB`);
+  }
+);
 
-var app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
 // Express validator
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-    var namespace = param.split('.'),
-    root          = namespace.shift(),
-    formParam     = root;
+app.use(
+  expressValidator({
+    errorFormatter: function(param, msg, value) {
+      var namespace = param.split("."),
+        root = namespace.shift(),
+        formParam = root;
 
-    while(namespace.lenght) {
-      formParam += '[' + namespace.shift() + ']';
+      while (namespace.lenght) {
+        formParam += "[" + namespace.shift() + "]";
+      }
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
     }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
-}));
+  })
+);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-//set static dir
-app.use(express.static(path.join(__dirname, 'public')));
-
 //routers
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
